f4f421daea74e29f1c3dda274341cc110f496c07
AngelDeveloper1 <metaliozzy@hotmail.com>
2018-08-22 09:35:08 -0500

    Se agrego codigo de operadores

---
+++ Operadores.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Operadores
+{
+	public class Complejo
+	{
+		public float Real       { get; set; }
+		public float Imaginario { get; set; }
+
+		public override string ToString()
+		{
+			string salida = String.Empty;
+			if (Real == 0f)
+				salida = String.Format("{0}i", Imaginario);
+			else if (Imaginario == 0f)
+				salida = String.Format("{0}", Real);
+			else
+				salida = String.Format("{0}{1}{2}i", 
+				                       Real,
+				                       Imaginario < 0f ? "-" : "+",
+				                       Math.Abs(Imaginario));       
+			return salida;
+		}
+		public static Complejo operator +(Complejo a,Complejo b)
+		{
+			return new Complejo() { Real = a.Real + b.Real,
+				Imaginario = a.Imaginario + b.Imaginario};
+			
+		}
+		public static Complejo operator -(Complejo a, Complejo b)
+        {
+            return new Complejo()
+            {
+                Real = a.Real - b.Real,
+                Imaginario = a.Imaginario - b.Imaginario
+            };
+
+        }
+		public static Complejo operator *(Complejo a, Complejo b)
+        {
+			return new Complejo()
+			{
+				Real = (a.Real * b.Real) - (a.Imaginario * b.Imaginario),
+				Imaginario = (a.Real * b.Imaginario) + (a.Imaginario * b.Real)
+            };
+
+        }
+		public static Complejo operator *(float escalar, Complejo b)
+        {
+            return new Complejo()
+            {
+                Real = (escalar * b.Real),
+                Imaginario = (escalar * b.Imaginario) 
+            };
+
+        }
+		public static Complejo operator *(Complejo b,float escalar)
+        {
+            return new Complejo()
+            {
+                Real = (escalar * b.Real),
+                Imaginario = (escalar * b.Imaginario)
+            };
+
+        }
+	}
+    class MainClass
+    {
+		
+        public static void Main(string[] args)
+        {
+			Complejo a = new Complejo() { Real = 12.34f, Imaginario = -45.67f };
+			Complejo b = new Complejo() { Real = 2.34f, Imaginario = 5.67f };
+            
+			Console.WriteLine( (65.7f*(a*b)+(a-b)*b)*5);
+
+        }
+    }
+}
